fea25fc7774bcc957a9f604eb950957a6074371b
Ankit <ankitymca27@gmail.com>
2026-03-23 15:35:34 +0530

    phase 1 testing developed

--- Assessment1/student_blog/blog/urls.py
+++ Assessment1/student_blog/blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('post/create/', views.post_create, name='post_create'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post/<slug:slug>/edit/', views.post_edit, name='post_edit'),
+    path('post/<slug:slug>/delete/', views.post_delete, name='post_delete'),
     path('my-posts/', views.my_posts, name='my_posts'),
     path('profile/<str:username>/', views.profile, name='profile'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
 ]
