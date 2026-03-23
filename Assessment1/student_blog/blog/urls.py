4b529d2f029d3a9350985ce9fcaf8db12884fcf9
Ankit <ankitymca27@gmail.com>
2026-03-23 14:15:19 +0530

    add post forntend pages profile pages along with working on models

--- Assessment1/student_blog/blog/urls.py
+++ Assessment1/student_blog/blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('my-posts/', views.my_posts, name='my_posts'),
+    path('profile/<str:username>/', views.profile, name='profile'),
 ]
--- Assessment1/student_blog/blog/views.py
+++ Assessment1/student_blog/blog/views.py
@@ -8,3 +8,15 @@ def login_view(request):
 
 def register_view(request):
     return render(request, 'blog/register.html')
+
+def post_detail(request, slug):
+    return render(request, 'blog/post_detail.html')
+
+def post_create(request):
+    return render(request, 'blog/post_form.html')
+
+def my_posts(request):
+    return render(request, 'blog/my_posts.html')
+
+def profile(request, username):
+    return render(request, 'blog/profile.html')
